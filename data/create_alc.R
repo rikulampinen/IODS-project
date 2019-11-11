e26cc9d9c916b83a2e4a7cee43dc006417b00b68
Richard Lamprecht <richla@uef.fi>
2019-11-11 15:32:05 +0200

    update 1 chapter 3

--- data/create_alc.R
+++ data/create_alc.R
@@ -12,7 +12,33 @@
 
 # 3.1. DATA WRANGLING ----
 
+# Read the data from the csv files
 
+mat <- read.csv(file = "C:/Users/richla/OneDrive/1 C - R-Folder/11-IODS-course/IODS-project/data/student-mat.csv", 
+                sep = ";", header = TRUE)
+
+por <- read.csv(file = "C:/Users/richla/OneDrive/1 C - R-Folder/11-IODS-course/IODS-project/data/student-por.csv", 
+                sep = ";", header = TRUE)
+
+# check the data sets
+
+# mat data set
+head(mat)
+dim(mat)
+str(mat)
+
+# por data set
+head(por)
+dim(por)
+str(por)
+
+
+
+d1=read.table("student-mat.csv",sep=";",header=TRUE)
+d2=read.table("student-por.csv",sep=";",header=TRUE)
+
+d3=merge(d1,d2,by=c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet"))
+print(nrow(d3)) # 382 students
 
 
 
